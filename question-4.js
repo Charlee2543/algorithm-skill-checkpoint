7bcb66c88f73e56cb302364d7d054e087184d318
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-04-29 11:33:26 +0700

    feat:answer question 4

--- question-4.js
+++ question-4.js
@@ -1,13 +1,24 @@
 function sortProductsByPrice(products) {
-  // Your code here
+   // Your code here
+   for (let i = 0; i < products.length; i++) {
+      for (let l = 0; l < products.length - i - 1; l++) {
+         //  console.log('products[l]: ', products[l]);
+         if (products[l].price > products[l + 1].price) {
+            const moreProduct = products[l];
+            products[l] = products[l + 1];
+            products[l + 1] = moreProduct;
+         }
+      }
+   }
+   return products;
 }
 
 // Test case
 const products = [
-  { name: "Apple", price: 1.2 },
-  { name: "Banana", price: 0.8 },
-  { name: "Cherry", price: 2.5 },
-  { name: "Date", price: 1.5 },
+   { name: 'Apple', price: 1.2 },
+   { name: 'Banana', price: 0.8 },
+   { name: 'Cherry', price: 2.5 },
+   { name: 'Date', price: 1.5 },
 ];
 console.log(sortProductsByPrice(products));
 
