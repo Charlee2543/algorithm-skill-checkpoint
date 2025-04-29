fb2815cae3e7e3e24c14ea6c43ea47fe72c22932
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-04-29 11:02:13 +0700

    feat:answer question 5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,7 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer:เป็นแบบ O(n กำลัง 2) เนื่องจากเป็นloopซ้อนloop fuction ของloopแรกคื่อการloopเปรียบเทียบราคาระหว่างตำแหน่งปัจจุบันกับราคาถัดไป เมื่อเปรียบเทียบเสร็จจะทำทำloopแรกในindex ที่1ต่อ 
+         ทำให้จำนวนรอบการทำงาน เป็นกำลัง 2 ขอปริมณข้อมูล 
 
 
 */
