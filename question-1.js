e14a027b73d1f50c2ba5a90fe555cb501a6bab3b
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-04-29 09:58:17 +0700

    feat:answer question 1

--- question-1.js
+++ question-1.js
@@ -1,14 +1,22 @@
 function findStudentById(students, id) {
-  // Your code here
+   // Your code here
+   const dataStudent = students.filter((student) => {
+      return student.id === id;
+   });
+   if (dataStudent.length) {
+      return dataStudent[0];
+   } else {
+      return null;
+   }
 }
 
 // Test case
 const students = [
-  { id: 302, name: "Emily Carter" },
-  { id: 105, name: "Michael Johnson" },
-  { id: 501, name: "Sarah Wilson" },
-  { id: 207, name: "Daniel Brown" },
-  { id: 403, name: "Sophia Martinez" },
+   { id: 302, name: 'Emily Carter' },
+   { id: 105, name: 'Michael Johnson' },
+   { id: 501, name: 'Sarah Wilson' },
+   { id: 207, name: 'Daniel Brown' },
+   { id: 403, name: 'Sophia Martinez' },
 ];
 
 console.log(findStudentById(students, 302)); // Output: { id: 302, name: "Emily Carter" }
